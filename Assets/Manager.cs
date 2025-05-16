cab08b8bff875ebe8eb56cb8716c181e0968a7d8
KotShredingera77 <riksancas7@gmail.com>
2025-05-16 23:49:12 +0300

    first

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public class Manager : MonoBehaviour
@@ -22,10 +24,18 @@ public class Manager : MonoBehaviour
 
     void Start()// вызывается перед обновлением первого кадра
     {
+        File.Create("Assets/Save1.txt").Close();
+        StreamWriter writer = new StreamWriter("Assets/Save1.txt", true);
+        GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag("CheckPoint");//Получить массив всех объектов по тегу
+        GameObject[] checkPoints = taggedObjects.OrderBy(go => go.name).ToArray();
+        for (int i = 0; i < checkPoints.Length; i++)
+        {
+            writer.WriteLine(checkPoints[i].name);
+        }
+        writer.Close();
 
-
-        //Debug.Log("Start");
-        if (populationSize % 2 != 0)
+            //Debug.Log("Start");
+            if (populationSize % 2 != 0)
             populationSize = 50;//если размер популяции нечетный, устанавливает его равным пятидесяти
 
         InitNetworks();
